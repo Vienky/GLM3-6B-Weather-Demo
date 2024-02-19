5faf8c7200d970f92e533f82e63ab000da46915d
Vienky <45895444+Vienky@users.noreply.github.com>
2024-02-19 17:07:33 +0800

    Create function_weather.py

---
+++ streamlit/function_weather.py
@@ -0,0 +1,69 @@
+import json
+from openai import OpenAI
+from weatherskill import get_weather, recommend_dress_by_weather, amap_weather_action
+
+client = OpenAI(
+    api_key = "xxxx",
+    base_url = "http://api.com/v1" #your own api url
+)
+
+function_map = {
+    "get_weather": get_weather
+}
+def run_conversation(user_query): #Here is an updation that your need put user_query to let the streamlit get input
+    MODEL = "chatglm3-6b"
+    messages = [
+            # {"role": "user", "content": "How's the weather like today in Beijing? Do you have any suggestions about clothing?"},
+            {"role": "user", "content": user_query},
+        ]
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=messages,
+        temperature=0,
+        functions=[
+            {
+                "name": "get_weather",
+                "description": "Get the current weather and get the weather recommendations",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "city": {
+                            "type": "string",
+                            "description": "City name for the weather query",
+                        },
+                    },
+                    "required": ["city"],
+                }
+            }
+        ],
+        function_call="auto",
+    )
+    #print(response)
+    respone_message = response.choices[0].message
+    #print(respone_message)
+
+    if hasattr(respone_message, "function_call"):
+        function_call = respone_message.function_call
+        function_name = function_call.name
+        arguments = json.loads(function_call.arguments)
+        #function_to_call = function_mapping.get(SkillFunctions[function_name], lambda **args: "Function not found")
+ 
+        function_response = function_map.get(function_name, lambda **args: "Function not found")(**arguments)
+  
+        messages.append(respone_message)
+        messages.append({
+            "role": "function",
+            "name": function_name,
+            "content": function_response,
+        })
+    else:
+        print("No function call in the response or response_message is None.")
+        
+    completion_final = client.chat.completions.create(
+        model = "chatglm3-6b",
+        messages = messages,
+    )
+
+    print(completion_final.choices[0].message.content)
+
+#run_conversation(xxx)
